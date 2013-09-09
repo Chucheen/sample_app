9d1914ff2b90d60c86c1d86e013184d75f9283f1
JGutierrez <jegut87@gmail.com>
2013-09-09 17:27:42 -0500

    Modifying to dynamic tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,10 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+    it "should have the right title" do
+      visit "/static_pages/home"
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 end
 
@@ -14,12 +18,22 @@ describe "Help page" do
     visit '/static_pages/help'
     page.should have_content('Help')
   end
+
+  it "should have the right title" do
+    visit "/static_pages/help"
+    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+  end
 end
 
 describe "About page" do
   it "should have the content 'About Us'" do
     visit '/static_pages/about'
     page.should have_content('About Us')
+  end
+
+  it "should have the right title" do
+    visit '/static_pages/about'
+    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About us")
   end
 end
 
