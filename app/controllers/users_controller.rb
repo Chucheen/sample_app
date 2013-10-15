135e8404407389a33261feb53c8cff5304144543
JGutierrez <jegut87@gmail.com>
2013-10-15 17:23:18 -0500

    User's sign-up finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # Handle a successful save
+      flash[:succes] = "¡Bienvenido #{@user.name.upcase}!"
+      redirect_to @user
     else
+      flash[:error] = "No se puede agregar el usuario #{@user.email}.<BR>Verifique los siguientes errores"
       render 'new'
     end
   end
