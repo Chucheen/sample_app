942490df53cdeeb7eb76d1967cbe374d375ad53c
JGutierrez <jegut87@gmail.com>
2013-10-25 18:43:06 -0500

    corrected bug with flash staying for 2 requests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,26 @@
 class SessionsController < ApplicationController
 
   def new
-
+    #@userName = params[:session][:email];;
   end
 
   def create
+    sessionHash = params[:session];
+    user = User.find_by_email sessionHash[:email]
+
+    if(user)
+      if(user.authenticate(sessionHash[:password]))
+        flash.now[:success] = "Welcome back #{user.name}<#{user.email}>"
+        sign_in user
+        render 'new'
+      else
+        flash.now[:error] = "Incorrect login: Bad password"
+        render 'new'
+      end
+    else
+      flash.now[:error] = "Incorrect login: User doesn't exist"
+      render 'new'
+    end
   end
 
   def destroy
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,10 +8,16 @@ describe "Ahthentication" do
 
     it {should have_selector('title', text: 'Sign in')}
 
-    describe "wiht invalid information" do
+    describe "with invalid information" do
       before {click_button "Sign in"}
       it {should have_selector('h1', text: 'Sign in')}
-      it {should have_selector('div.alert.alert-error', text: 'Invalid')}
+      it {should have_selector('div.alert.alert-error', text: 'Incorrect login:')}
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+
     end
 
     describe "with valid information" do
