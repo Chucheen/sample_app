9a99b88ebf74d86a1e9552c3e917a92842e52031
JGutierrez <jegut87@gmail.com>
2014-02-05 13:32:52 -0600

    Setted a characters countdown for microposts and word wrap too

--- app/assets/javascripts/micropost.js
+++ app/assets/javascripts/micropost.js
@@ -1,4 +1,8 @@
 /**
  * Created by jgutierrez on 31/01/14.
  */
-$('#char_labels')
+$(document).ready(function(){
+    $('#micropost-content').keyup(function(){
+        $('#micropost-characters').text((140 - $('#micropost-content').val().length) + ' characters left');
+    });
+});
